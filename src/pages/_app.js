1056076da03af16df86f55461feb38798ce29816
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:21:38 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,5 +3,6 @@ import { AnimatePresence } from "framer-motion";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+    <Component {...pageProps} />;
 }
