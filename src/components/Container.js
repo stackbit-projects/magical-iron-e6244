a9c0ee1a93c894ce1ef43473c73c58d0e0466e58
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:06:07 +0000

    Container.js: changed

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import React  from "react";
 
 
-export default class BlogPostTags extends React.Component {
+export default class Container extends React.Component {
   return (
     <div className={clsx("px-4 mx-auto max-w-screen-md", className)}>
       {children}
