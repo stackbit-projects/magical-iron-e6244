8fb9c9efc69914d63b0ac447c6b841c49f28761d
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 14:43:43 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-
+import { motion } from 'framer-motion';
 
 import components, { Layout } from '../components/index';
 import { getPageUrl } from '../utils';
