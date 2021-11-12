297d086a1ed67e396ea0869c25a2750fc32f9d4e
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:09:09 +0000

    Container.js: changed

--- src/components/Container.js
+++ src/components/Container.js
@@ -11,5 +11,6 @@ export default class Container extends React.Component {
         <div className={clsx("px-4 mx-auto max-w-screen-md", className)}>
         {children}
         </div>
-    );
+        );
+        
 };
