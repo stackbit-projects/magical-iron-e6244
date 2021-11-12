b947ec3c08085e560f81fe8fd6875c00c5f5f546
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 14:55:53 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -16,17 +16,17 @@ export default class Advanced extends React.Component {
 
         return (
             const [citation, setCitation] = useState(true);
-  const [main, setMain] = useState(false);
-  const { t } = useTranslation("common");
+            const [main, setMain] = useState(false);
+            const { t } = useTranslation("common");
 
-  useEffect(() => {
-    const ids = [
-      setTimeout(() => setCitation(false), 4800),
-      setTimeout(() => setMain(true), 5700),
-    ];
+            useEffect(() => {
+                const ids = [
+                setTimeout(() => setCitation(false), 4800),
+                setTimeout(() => setMain(true), 5700),
+                ];
 
-    return () => ids.forEach((id) => clearTimeout(id));
-  }, [setCitation]);    
+                return () => ids.forEach((id) => clearTimeout(id));
+            }, [setCitation]);    
             
             <Layout page={page} config={config}>
                 {_.map(sections, (section, index) => {
