3d28199739646552eb5cca815126efb430a38dba
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:08:44 +0000

    Container.js: changed

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,11 +5,11 @@ import { Props } from "./props";
 
 export default class Container extends React.Component {
      render() {
-    const className = _.get(this.props, 'className');
-    const children = _.get(this.props, 'children');
-    return (
-    <div className={clsx("px-4 mx-auto max-w-screen-md", className)}>
-      {children}
-    </div>
+        const className = _.get(this.props, 'className');
+        const children = _.get(this.props, 'children');
+        return (
+        <div className={clsx("px-4 mx-auto max-w-screen-md", className)}>
+        {children}
+        </div>
   );
 };
