0785a6c411e82a81290625356c05f194dfef7240
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:24:15 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
 import '../sass/main.scss';
-import { AnimatePresence } from "framer-motion";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return (
-    <AnimatePresence exitBeforeEnter>
     <Component {...pageProps} />
-    </AnimatePresence>
     );
 }
