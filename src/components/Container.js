f703d9b2f6cd8bf8891379aaadb8cc7b24c61905
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:06:47 +0000

    Container.js: changed

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,9 @@
 import clsx from "clsx";
 import React  from "react";
 
+import { Props } from "./props";
 
-export default class Container extends React.Component {
+export const Container: FC<Props> = ({ className, children }: Props) => {
   return (
     <div className={clsx("px-4 mx-auto max-w-screen-md", className)}>
       {children}
