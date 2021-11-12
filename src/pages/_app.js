a040bc1989af282fbf246346e91abe8b35523345
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 15:32:28 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,9 @@ import '../sass/main.scss';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return (
+        <AnimatePresence exitBeforeEnter>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
     <Component {...pageProps} />
     );
 }
