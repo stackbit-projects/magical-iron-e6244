0b1781e325c424ac9504fc6d24f4f86288441007
Stackbit Code Editor <projects@stackbit.com>
2021-12-14 07:35:32 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -91,6 +91,8 @@ export default class Footer extends React.Component {
                         </div>
                     </div>
                 )}
+                  <div>AS SEEN ON <span><br></br><img src="https://nftcalendar.io/assets/logo/nftcalendar-logo.png" alt="Kitten"
+	title="A cute kitten" width="50" height="50" /></span></div>
             </footer>
         );
     }
